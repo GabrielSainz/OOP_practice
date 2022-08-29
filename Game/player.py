1d163f8eec69d41ccf9dbe54f42827a24327b644
Gabriel Sainz <gs.sainz08@hotmail.com>
2022-08-28 19:27:58 -0500

    property exercises

--- Game/player.py
+++ Game/player.py
@@ -3,7 +3,7 @@ class Player(object):
     def __init__(self, name):
         self.name = name 
         self._lives = 3
-        self.level = 1
+        self._level = 1
         self.score = 0
     
     def _get_lives(self): 
@@ -16,7 +16,18 @@ class Player(object):
             print('Lives cannot be negative')
             self._lives = 0
 
+    def _get_level(self): 
+        return self._level
+    
+    def _set_level(self, level): 
+        if level < 1: 
+            print('Level cannot be less than 1')
+        else: 
+            self.score += 1000 * (level - self._level)
+            self._level = level
+
     lives = property(_get_lives, _set_lives)
+    level = property(_get_level, _set_level)
 
     def __str__(self): 
         return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score: {0.score}".format(self)
