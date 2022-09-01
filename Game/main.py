fdbe9113685e5938c473c3cf99ddae207cf656a7
Gabriel Sainz <gs.sainz08@hotmail.com>
2022-08-31 19:02:22 -0500

    add new files

--- Game/main.py
+++ Game/main.py
@@ -27,3 +27,23 @@ print(tim)
 
 tim.level = 3
 print(tim)
+
+tim.score = 500
+print(tim)
+
+
+
+# Exec and eval functions. 
+# 1. eval accepts only a single expression, exec can take a code block that has Python 
+#   statements: loop, try: except, class and function/method def initions and so on. 
+# 2. eval returns the value of the given expression, whereas exec ignores the return value 
+#   from its code, and always returns None. 
+
+
+x = 1
+
+a = eval("x+1")
+print(a)
+
+a = exec("x+1")
+print(a)
